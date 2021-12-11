581d131c1a2b90e45350b5d13f95251e9bb71906
MauricioCaceres <david_mauricio_caceres@hotmail.com>
2021-12-10 21:39:05 -0500

    Proyecto sprint4

--- Proyecto/src/app/Pages/pages.module.ts
+++ Proyecto/src/app/Pages/pages.module.ts
@@ -5,14 +5,22 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { UserComponent } from './user/user.component';
+import { AgregarComponent } from './admin/agregar/agregar.component';
+import { SuprComponent } from './admin/supr/supr.component';
+import { ModificarComponent } from './admin/modificar/modificar.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    HomeComponent,
     AdminComponent,
-    UserComponent
+    UserComponent,
+    AgregarComponent,
+    SuprComponent,
+    ModificarComponent,
+    
   ],
   imports: [
     CommonModule,
